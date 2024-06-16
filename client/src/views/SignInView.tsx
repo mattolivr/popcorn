3a90071f3211faeff579aa443c3bae3632b3188a
Matheus de Oliveira <matheusdeoliveira310@gmail.com>
2024-06-16 12:00:56 -0300

    feat: Ajustes na tela de Cadastro

--- client/src/views/SignInView.tsx
+++ client/src/views/SignInView.tsx
@@ -2,7 +2,7 @@ import { Avatar } from "flowbite-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaEnvelope } from "react-icons/fa";
-import { FaAt, FaCakeCandles, FaImage, FaImagePortrait, FaKey, FaUser } from "react-icons/fa6";
+import { FaCakeCandles, FaImage, FaImagePortrait, FaKey, FaUser } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 import Anchor from "../components/Anchor.tsx";
 import Button from "../components/button/Button.tsx";
@@ -13,6 +13,14 @@ import { InputFile } from "../components/input/type/InputFile.tsx";
 import { InputText } from "../components/input/type/InputText.tsx";
 import DialogLayout from "./layouts/DialogLayout";
 
+enum Steps {
+  LOGIN = 1,
+  GENERAL = 2,
+  PASSWORD = 3,
+  PROFILE = 4,
+  SUBMIT = 5,
+}
+
 export default function SignInView(): React.ReactNode {
   const { register, handleSubmit, formState } = useForm();
   const onSubmit = handleSubmit((data) => {
@@ -20,8 +28,17 @@ export default function SignInView(): React.ReactNode {
   });
 
   const [step, setStep] = useState(1);
-  const totalSteps = 4;
-  const showAtStep = (s: number): boolean => s !== step;
+  const totalSteps = Object.keys(Steps).length / 2;
+  const showAtStep = (...steps: number[]): boolean => {
+    let show = false;
+    steps.forEach((s) => {
+      if (s === step) {
+        show = true;
+      }
+    });
+    return show;
+  };
+
   const nextStep = (): void => {
     setStep(step + 1);
   };
@@ -31,6 +48,7 @@ export default function SignInView(): React.ReactNode {
 
   const titles: string[] = [
     "Criar uma nova conta",
+    "Preencha suas informações",
     "Formule uma senha",
     "Personalize o perfil",
     "Confirme as informações",
@@ -45,57 +63,57 @@ export default function SignInView(): React.ReactNode {
         className="flex flex-col gap-2"
       >
         <Input
-          name="username"
-          label={<Input.Label text="Nome" />}
+          name="tag"
+          label={<Input.Label text="Nome de Usuário" />}
           type={<InputText />}
           icon={<Input.Icon icon={FaUser} />}
-          hidden={showAtStep(1)}
-        />
-        <Input
-          name="usertag"
-          label={<Input.Label text="Tag de Usuário" />}
-          type={<InputText />}
-          icon={<Input.Icon icon={FaAt} />}
-          hidden={showAtStep(1)}
+          hidden={!showAtStep(Steps.LOGIN)}
         />
         <Input
           name="email"
           label={<Input.Label text="Email" />}
           type={<InputText />}
           icon={<Input.Icon icon={FaEnvelope} />}
-          hidden={showAtStep(1)}
+          hidden={!showAtStep(Steps.LOGIN)}
+        />
+        <Input
+          name="name"
+          label={<Input.Label text="Nome" />}
+          type={<InputText />}
+          icon={<Input.Icon icon={FaUser} />}
+          hidden={!showAtStep(Steps.GENERAL)}
         />
         <Input
           name="birthday"
           label={<Input.Label text="Data de Nascimento" />}
           type={<InputText />}
           icon={<Input.Icon icon={FaCakeCandles} />}
-          hidden={showAtStep(1)}
+          hidden={!showAtStep(Steps.GENERAL)}
         />
         <Input
           name="password"
           label={<Input.Label text="Senha" />}
           type={<InputText />}
           icon={<Input.Icon icon={FaKey} />}
-          hidden={showAtStep(2)}
+          hidden={!showAtStep(Steps.PASSWORD)}
         />
         <Input
           name="passwordConfirm"
           label={<Input.Label text="Confirme a Senha" />}
           type={<InputText />}
           icon={<Input.Icon icon={FaKey} />}
-          hidden={showAtStep(2)}
+          hidden={!showAtStep(Steps.PASSWORD)}
         />
         <ProfilePreview
-          previewHidden={showAtStep(3) && showAtStep(4)}
-          userDataHidden={showAtStep(4)}
+          previewHidden={!showAtStep(Steps.PROFILE, Steps.SUBMIT)}
+          userDataHidden={!showAtStep(Steps.SUBMIT)}
         />
         <Input
           name="photo"
           label={<Input.Label text="Foto de Perfil" />}
           type={<InputFile />}
           icon={<Input.Icon icon={FaImagePortrait} />}
-          hidden={showAtStep(3)}
+          hidden={!showAtStep(Steps.PROFILE)}
           optional
         />
         <Input
@@ -103,20 +121,20 @@ export default function SignInView(): React.ReactNode {
           label={<Input.Label text="Fundo de Perfil" />}
           type={<InputFile />}
           icon={<Input.Icon icon={FaImage} />}
-          hidden={showAtStep(3)}
+          hidden={!showAtStep(Steps.PROFILE)}
           optional
         />
-        <Anchor to="/login" hidden={showAtStep(1)}>
+        <Anchor to="/login" hidden={!showAtStep(Steps.LOGIN)}>
           Já tem uma conta? Faça Login
         </Anchor>
-        <Button onClick={previousStep} color="transparent" hidden={!showAtStep(1)}>
+        <Button onClick={previousStep} color="transparent" hidden={showAtStep(Steps.LOGIN)}>
           Anterior
         </Button>
-        <Button onClick={nextStep} hidden={!showAtStep(4)}>
+        <Button onClick={nextStep} hidden={showAtStep(totalSteps)}>
           Próximo
         </Button>
-        <Form.Submit hidden={showAtStep(totalSteps)}>Confirmar cadastro</Form.Submit>
-        <LoginRedirect hidden={showAtStep(1)} />
+        <Form.Submit hidden={!showAtStep(totalSteps)}>Confirmar cadastro</Form.Submit>
+        <LoginRedirect hidden={!showAtStep(1)} />
         <Stepper totalSteps={totalSteps} step={step} setStep={setStep} />
       </Form>
     </DialogLayout>
