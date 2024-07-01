142cee881e14c591391be91cf8e384697149efad
Matheus de Oliveira <matheusdeoliveira310@gmail.com>
2024-06-30 22:02:05 -0300

    feat: criação do servidor back-end

---
+++ server/src/test/java/com/mattolivr/popcorn/PopcornClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattolivr.popcorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
